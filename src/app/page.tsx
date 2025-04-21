d57ddd5638994e88a7f85c0f23ea06db3534cfb4
andthedropout <andthedropout@gmail.com>
2025-04-20 23:14:56 -0700

    Init CMS setup

---
+++ src/app/page.tsx
@@ -0,0 +1,11 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      {/* Content will be added here */}
+      <h1>Landing Page Template</h1>
+
+    </main>
+  );
+}
