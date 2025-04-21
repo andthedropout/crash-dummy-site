c67dc9a1d21cbf38a3a5ca1027d91d9f3ae885a9
andthedropout <andthedropout@gmail.com>
2025-04-20 23:18:24 -0700

    Fix: Remove unused Image import causing build failure"

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,9 @@
-import Image from "next/image";
-
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       {/* Content will be added here */}
       <h1>Landing Page Template</h1>
-
+      <p>Edit src/app/page.tsx to start building.</p>
     </main>
   );
 }
